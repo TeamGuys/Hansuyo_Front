3d255da1de57fdd5fdfb6a8ed593ac97e504cb99
ys0222 <ggdbstjd0222@gmail.com>
2024-07-28 15:26:28 +0900

    Delete : 홈 배경화면 삭제

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import Album from "../../components/Home/Album"
 
 const Home = () => {
   return (
-    <div className="h-[100vh] bg-slate-500 p-1 ">
+    <div className="h-[100vh] p-1 ">
       {/* 검색창 상단바 */}
       <div className="flex h-[50px] p-1 space-x-3" >
         <SearchInput />
