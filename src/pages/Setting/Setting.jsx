f4c07df484efb3eeaff36cf75fffa937d93b80e0
ys0222 <ggdbstjd0222@gmail.com>
2024-07-29 21:26:13 +0900

    Feat : 나의 구독채널 구현

--- src/pages/Setting/Setting.jsx
+++ src/pages/Setting/Setting.jsx
@@ -2,12 +2,23 @@ import React from "react"
 
 import { Nav } from "../../components/Setting/Nav/Nav"
 import {Profile} from "../../components/Setting/Profile/Profile"
+import {Border} from "../../components/Setting/Border"
+import { Subscribe } from "../../components/Setting/Subscirbe/Subscribe"
+
 const Setting = () =>{
   return(
     <>
       <Nav />
       
       <Profile />
+
+      <Border />
+        <Subscribe />
+      <Border />
+
+
+      <Border />
+
     </>
   )
 }
