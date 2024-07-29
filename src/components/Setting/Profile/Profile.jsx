86ee7a3ce21b747d557a3891ebcde01c11fecaeb
ys0222 <ggdbstjd0222@gmail.com>
2024-07-29 20:57:42 +0900

    Feat : 한줄소개 구현

--- src/components/Setting/Profile/Profile.jsx
+++ src/components/Setting/Profile/Profile.jsx
@@ -1,16 +1,23 @@
 import ProfileImage from "./ProfileImage";
 import Advice from "./Advice";
+import Comment from "./Comment";
 
 export const Profile = () => {
   return (
-    <div className="flex justify-between w-full py-3 px-[60px]">
-      <div>
-        <ProfileImage />
+    <>
+      <div className="flex justify-between w-full py-3 px-[60px]">
+        <div>
+          <ProfileImage />
+        </div>
+        <div className="flex flex-col justify-center space-y-2">
+          <Advice text={'받은'} />
+          <Advice text={'한'} />
+        </div>
+
       </div>
-      <div className="flex flex-col justify-center space-y-2">
-        <Advice text={'받은'} />
-        <Advice text={'한'} />
+      <div className="w-full px-[60px]">
+        <Comment comment={'comment'} />
       </div>
-    </div>
+    </>
   );
 }
