6f1bcf39dadeefb4fa7a5a350f5e795b98dccc4b
ys0222 <ggdbstjd0222@gmail.com>
2024-08-01 02:16:30 +0900

    Design : 게시판 메시지 디자인 수정

--- src/components/Board/Message.jsx
+++ src/components/Board/Message.jsx
@@ -2,7 +2,9 @@ import Messages from "../../assets/Home/Message.svg"
 
 const Message = ({setOpenCommentModal}) => {
   return(
-    <div onClick={setOpenCommentModal}>
+    <div 
+      className="flex flex-col items-center justify-center"
+      onClick={setOpenCommentModal}>
       <img src={Messages} alt='x' />
       <p className="font-[#000"> 55 </p>
     </div>
